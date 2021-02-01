c3378d5f13e5db0011d3465562477863f72747da
Kallahan Salas <77999591+Kallahans21@users.noreply.github.com>
2021-02-01 10:57:09 -0600

    Update Program.cs

--- EJERCICIO1/Program.cs
+++ EJERCICIO1/Program.cs
@@ -12,36 +12,36 @@ namespace EJERCICIO1
         {
             Console.Title = "Resultados del test de capacitación";
             Console.WriteLine("Bienvenido a la calculadora de resultados del test de capacitación. \nA continuación, rellene lo que se le solicite para obtener los resultados y el nivel alcanzado");
-            // T: preguntas totales
-            // C: preguntas contestadas
-            // R: resultados de porcentaje 
+            // pregTotales: preguntas totales
+            // contestadas: preguntas contestadas
+            // Result: resultados de porcentaje 
             // A: Lector de datos (argumento para int) 
-            Int32 T, C, R;
+            Int32 pregTotales, contestadas, Result;
             String A;
             Console.WriteLine("\nDigite el número de preguntas totales:\n");
             A = Console.ReadLine();
-            T=  Int32.Parse(A);
+            pregTotales=  Int32.Parse(A);
             Console.WriteLine("\nDigite el número de preguntas contsetadas:\n");
             A = Console.ReadLine();
-            C = Int32.Parse(A);
+            contestadas = Int32.Parse(A);
 
-            R = (C * 100) / T;
+            Result = (contestadas * 100) / pregTotales;
 
-            if (R >= 90)
+            if (Result >= 90)
             {
-                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel máximo. \nHas obtenido el  " + R + "%");
+                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel máximo. \nHas obtenido el  " +Result + "%");
             } 
-            if (R>=75 && R<90)
+            if (Result>=75 && Result<90)
             {
-                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel medio. \nHas obtenido el  " + R + "%");
+                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel medio. \nHas obtenido el  " + Result + "%");
             }
-            if (R >= 50 && R<75)
+            if (Result>= 50 && Result<75)
             {
-                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel regular. \nHas obtenido el  " + R + "%");
+                Console.WriteLine("\n¡Felicidades! \nEl nivel obtenido es el nivel regular. \nHas obtenido el  " + Result + "%");
             }
-            else if (R <50)
+            else if (Result <50)
             {
-                Console.WriteLine("\n¡Más suerte a la próxima! \nTus resultados están fuera de nivel. \nHas obtenido el  " + R + "%");
+                Console.WriteLine("\n¡Más suerte a la próxima! \nTus resultados están fuera de nivel. \nHas obtenido el  " + Result + "%");
             }
             Console.ReadKey();
 
